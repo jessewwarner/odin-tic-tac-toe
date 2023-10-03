c612e0c6df704a7b5c2ee099bd1aa8491f372665
Jesse Warner <jesse.w.warner@gmail.com>
2023-10-03 11:20:21 -0400

    Adding winning logic

--- js/script.js
+++ js/script.js
@@ -3,21 +3,69 @@ const closeBtn = document.querySelector('.close-btn');
 const oppPlayerBtn = document.querySelector('.player-btn');
 const oppCompBtn = document.querySelector('.computer-btn');
 
+const playSquares = document.querySelectorAll('.play-square');
+
 const rulesWindow = document.querySelector('.rules-wrapper');
+const selectOpponentWindow = document.querySelector('.select-opponent');
+const gameBoardWindow = document.querySelector('.game-board');
+const winMsgWindow = document.querySelector('.win-msg');
 
 const Player = (name, symbol) => {
     return {name, symbol};
 };
 
+const playerOne = Player('Player 1', 'blue');
+let playerTwo;
+
 const GameBoard = (() => {
     const grid = [0,0,0,0,0,0,0,0,0];
-    const addPiece = (index, currentPlayer) => {
+    const addPiece = (index, currentPlayer, square) => {
         if (grid[index] !== 0) return;
         grid[index] = currentPlayer.symbol === 'blue' ? 1 : 2;
+        square.classList.add(`${currentPlayer.symbol}-player`);
+        GameLogicController.checkForWin(grid);
+        GameLogicController.switchPlayer();
+        console.log(grid); // REMOVE ME
     };
     const getGridState = () => grid;
+    return {addPiece, getGridState};
 })();
 
+const GameLogicController = (() => {
+    let currentPlayer = playerOne;
+    const switchPlayer = () => {
+        currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+    };
+    const getCurrentPlayer = () => currentPlayer;
+
+    const selectOpponent = (name) => {
+        playerTwo = Player(name, 'red');
+        selectOpponentWindow.classList.toggle('hide');
+        gameBoardWindow.classList.toggle('hide');
+    };
+
+    const checkForWin = (grid) => {
+        let winner = false;
+        const winningCombos = [[0,1,2],[3,4,5],[6,7,8],
+                                [0,3,6],[1,4,7],[2,5,8],
+                                [0,4,8],[2,4,6]];
+                                
+        winningCombos.forEach((combo) => {
+            if (combo.every(value => grid[value] === 1)) {
+                winner = true;
+            } else if (combo.every(value => grid[value] === 2)) {
+                winner = true;
+            } 
+        });
+        if (winner === false){
+            if (grid.every(value => value !== 0)){
+                console.log("It's a draw!");
+            }
+        }
+    }
+
+    return {switchPlayer, getCurrentPlayer, selectOpponent, checkForWin}
+})();
 
 rulesBtn.addEventListener('click', () => {
     rulesWindow.classList.toggle('hide');
@@ -28,5 +76,18 @@ closeBtn.addEventListener('click', () => {
 });
 
 oppPlayerBtn.addEventListener('click', () => {
-    opponent
+    GameLogicController.selectOpponent('Player 2');
+
+});
+
+oppCompBtn.addEventListener('click', () => {
+    GameLogicController.selectOpponent('Computer');
+});
+
+playSquares.forEach(square => {
+    square.addEventListener('click', () => {
+        const index = parseInt(square.getAttribute('data-index'));
+        const boardPosition = square.firstElementChild;
+        GameBoard.addPiece(index, GameLogicController.getCurrentPlayer(), boardPosition);
+    });
 });
